d85ffdffa6039ff67ce0576626b952a224184012
Justin Wyer <justin@lifeasageek.com>
2014-11-03 12:40:05 +0200

    Use secure websocket

--- client/main/game.controller.js
+++ client/main/game.controller.js
@@ -18,7 +18,7 @@
         });
         $scope.setName = function (name) {
             $scope.player = name;
-            $scope.client = new GameClient('ws://' + $location.host() + ':' + $location.port() + '/quiz', $scope.player);
+            $scope.client = new GameClient('wss://' + $location.host() + ':' + $location.port() + '/quiz', $scope.player);
             $scope.client.on('searching', function () {
                 $scope.gameState = 'searching';
                 $scope.$apply();
